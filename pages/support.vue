bdfe5dda1267fd735361b782109f45bedae6617b
Jonathan Dozier-Ezell <jdozier-ezell@afsp.org>
2018-06-20 07:42:44 -0400

    adding support.vue

---
+++ pages/support.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container container--narrow content-page">
+    <h1>{{ heading }}</h1>
+    <div class="flexbox">
+      <script src="https://cdn.donately.com/dntly-core/current/core.min.js" data-donately-id="act_be92c26650f4" data-stripe-publishable-key="pk_live_eser55FwsexB5JBx9x0zT2aQ" data-donately-presets="100,50,25,15" data-donately-duration="default_recurring" data-donately-billing-zip="true" data-donately-phone="true" data-donately-anonymous="true" data-donately-onbehalf="true" data-donately-payment-options="cc,ach" ></script>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      heading: 'Thank you for supporting Making Gay History!',
+    }
+  },
+  transition: 'fade'
+}
+</script>
