89c16f7c19c27bb2fb1c524fcd9a4b51c9854899
American Foundation for Suicide Prevention <afsp@Americans-MacBook-Pro.local>
2018-12-13 10:36:39 -0500

    restore icloud

--- pages/support.vue
+++ pages/support.vue
@@ -11,7 +11,7 @@
 export default {
   data () {
     return {
-      heading: 'Thank you for supporting Making Gay History!',
+      heading: 'Thank you for supporting MGH!',
     }
   },
   transition: 'fade'
