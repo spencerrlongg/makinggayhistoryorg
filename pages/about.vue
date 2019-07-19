db8894c1c609fe3d02922231cecad563cf7c879c
jlunardini <john-carlos.lunardini@mikestechshop.com>
2019-07-19 10:52:12 -0400

    textual changes.

--- pages/about.vue
+++ pages/about.vue
@@ -7,10 +7,6 @@
       <p v-html="body[0]"></p>
     </div>
     <hr />
-    <div class="flexbox">
-      <img src="~assets/img/Sara-Headshot-768x768.jpg" :alt="imageAlt[1]">
-      <p v-html="body[1]"></p>
-    </div>
   </div>
 </template>
 
@@ -21,7 +17,7 @@ export default {
       heading: 'Making Gay History',
       cta: 'Need our help?  <a href="mailto:hello@makinggayhistory.org">Get in touch.</a>',
       imageAlt: [ 'Headshot of Eric Marcus', 'Headshot of Sara Burningham' ],
-      body: [ '<strong>Eric Marcus</strong> is the creator and host of the Making Gay History podcast.  He is the author of a dozen books, including Making Gay History, Is It A Choice?, Why Suicide?, and Breaking the Surface, the #1 New York Times bestselling autobiography of Olympic diving champion Greg Louganis.', '<strong>Sara Burningham</strong> is a New York-based radio producer, editor, and writer.  She has produced award-winning FM talk radio in London, reported on the U.S. elections as a foreign correspondent, crafted narrative gems for public radio in the Ozarks, edited the rat-a-tat-tat radio AM News leader that is 1010WINS, and now has the privilege of gleaning life-changing stories from the Making Gay History archive.' ]
+      body: [ '<strong>Eric Marcus</strong> is the founder and host of the Making Gay History podcast.  He is the author and co-author of a dozen books, including <i>Making Gay History, Is It A Choice?, Why Suicide?,</i> and <i>Breaking the Surface,</i> the #1 New York Times bestselling autobiography of Olympic diving champion Greg Louganis.' ]
     }
   },
   transition: 'fade'
--- pages/education.vue
+++ pages/education.vue
@@ -14,7 +14,7 @@ export default {
     return {
       heading: 'Education',
       imageAlt: 'Logo for History UnErased',
-      body: 'Making Gay History works in partnership with <a href="https://unerased.org/" target="_blank">History UnErased</a> to bring LGBTQ-inclusive curricula to middle- and high-school classrooms in New York City and other school districts across the country.<br /><br /><a href="http://www.reuters.com/article/us-usa-lgbt-education-idUSKBN18T1AP" target="_blank">Read more about our work here.</a>'
+      body: 'Making Gay History works in partnership with <a href="https://unerased.org/" target="_blank">History UnErased</a> to create LGBTQ-inclusive teaching resources for middle and high school classrooms.<br /><br /><a href="http://www.reuters.com/article/us-usa-lgbt-education-idUSKBN18T1AP" target="_blank">Read this Reuters article about our work</a> and explore the available teaching materials <a href="https://www.makinggayhistory.org/classroom/" target="_blank"> here,</a><a href="https://unerased.org/mgh" target"_blank"> here,</a> <a href="https://unerased.org/resource/nyc" target="_blank"> and here.</a><br /><br /><a href="https://www.facinghistory.org/" target="_blank">Facing History and Ourselves L.A.</a> has also developed teaching prompts and resources anchored to several Making Gay History episodes. You can find them <a href="http://lanetwork.facinghistory.org/topic/lgbtq" target="_blank">here.</a>'
     }
   },
   transition: 'fade'
