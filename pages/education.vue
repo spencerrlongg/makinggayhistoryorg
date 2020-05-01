4eeac7ef9895688e97565fb5776e975244da8eb5
jlunardini <john-carlos.lunardini@mikestechshop.com>
2020-05-01 16:39:22 -0400

    small change to /education

--- pages/education.vue
+++ pages/education.vue
@@ -14,7 +14,7 @@ export default {
     return {
       heading: 'Education',
       imageAlt: 'Logo for History UnErased',
-      body: 'Making Gay History works in partnership with <a href="https://unerased.org/" target="_blank">History UnErased</a> to create LGBTQ-inclusive teaching resources for middle and high school classrooms.<br /><br /><a href="http://www.reuters.com/article/us-usa-lgbt-education-idUSKBN18T1AP" target="_blank">Read this Reuters article about our work</a> and explore the available teaching materials <a href="https://www.makinggayhistory.org/classroom/" target="_blank">here</a>, <a href="https://unerased.org/mgh" target"_blank">here</a>, and <a href="https://unerased.org/resource/nyc" target="_blank">here</a>.<br /><br /><a href="https://www.facinghistory.org/" target="_blank">Facing History and Ourselves</a> L.A. has also developed teaching prompts and resources anchored to several Making Gay History episodes. You can find them <a href="http://lanetwork.facinghistory.org/topic/lgbtq" target="_blank">here</a>.'
+      body: 'Making Gay History works in partnership with <a href="https://unerased.org/" target="_blank">History UnErased</a> to create LGBTQ-inclusive teaching resources for middle and high school classrooms.<br /><br /><a href="http://www.reuters.com/article/us-usa-lgbt-education-idUSKBN18T1AP" target="_blank">Read this Reuters article about our work</a>.<br /><br /<a href="https://www.facinghistory.org/" target="_blank">Facing History and Ourselves</a> L.A. has also developed teaching prompts and resources anchored to several Making Gay History episodes. You can find them <a href="http://lanetwork.facinghistory.org/topic/lgbtq" target="_blank">here</a>.'
     }
   },
   transition: 'fade'
